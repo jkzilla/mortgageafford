6183f03fc88b419abed84d9eaf27f3f18309f08d
johannakate <jkgriffin@ucdavis.edu>
2015-10-24 20:27:40 -0700

    updates

--- static/js/form.js
+++ static/js/form.js
@@ -1,31 +1,35 @@
+var main = function() {
 
-
-$(document).ready(
 	var form = $('#myForm');
+	
 	form.validate();
+	
 	$('#Submit').click(function() {
+	
 	console.log('hi')
-	var $annualIncome = $("#annualIncome").val();
-	var $monthlyPayment = $("#monthlyPayment").val();
-	var $down = $("#down").val();
+	
+	var $annualIncome = $('#annualIncome').val();
+	var $monthlyPayment = $('#monthlyPayment').val();
+	var $down = $('#down').val();
 
-	console.log(monthlyPayment);
-	console.log(annualIncome);
-	if $annualIncome.length != 0 {
+	if ($annualIncome != null) {
 		return true;
 	}
-	else if $annualIncome.length === 0 && $monthlyPayment.length != 0 {
+	else if ($annualIncome.length === 0 && $monthlyPayment.length != 0) {
 		// submit form
 		return true;
 	}
-	else if $annualIncome.length && $monthlyPayment.length === 0 {
+	else if ($annualIncome.length && $monthlyPayment.length === 0) {
 		alert("Please enter either an annual income or a monthly payment.");
 		return false;
 	}
-	else if $down.length === 0 {
-		alert(Please enter the amount of your down payment);
+	else if ($down.length === 0) {
+		alert("Please enter the amount of your down payment.");
 		return false;
 	}
 	// do not submit form
 	return true;
-});
+}
+);
+
+$(document).ready(main);
