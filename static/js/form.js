958727fd2e122016bd5d95e6c4a76533a4199d14
johannakate <jkgriffin@ucdavis.edu>
2015-10-24 19:03:00 -0700

    jquery changes

--- static/js/form.js
+++ static/js/form.js
@@ -3,13 +3,23 @@
 $(document).ready(function validateForm() {
 	console.log('hi')
 	
-	var annualIncome = $("#annualIncome").val();
-	var monthlyPayment = $("#monthlyPayment").val();
+	var $annualIncome = $("#annualIncome").val();
+	var $monthlyPayment = $("#monthlyPayment").val();
 	console.log(monthlyPayment);
 	console.log(annualIncome);
-	if annualIncome || monthlyPayment {
-		return true
+	if $annualIncome.length != 0 {
+		return true;
 	}
-	return false
+	else if $annualIncome.length === 0 && $monthlyPayment.length != 0 {
+		// submit form
+		return true;
+	}
+	else if $annualIncome.length && $monthlyPayment.length === 0 {
+		alert("Please enter either an annual income or a monthly payment.")
+		return false;
+	}
+	// do not submit form
+	alert("Please fill out values correctly");
+	return false;
 }
 )
